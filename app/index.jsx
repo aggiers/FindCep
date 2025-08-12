9f4d1fc6d014f765bc06e5fa6676c459df47b86d
Agatha <agatha.g@aluno.senai.br>
2025-08-12 15:04:08 -0300

    :tada: Enviando projeto com a tela de consulta de CEP em desenvolvimento

---
+++ app/index.jsx
@@ -0,0 +1,54 @@
+
+import { Image, ImageBackground, StyleSheet, View, Text } from "react-native";
+import {Input} from '../components/input/input'
+
+export default function Index() {
+  return (
+    <>
+      {/* 1. logo + imagem de fundo */}
+      <ImageBackground source={require('../assets/images/ImgFundo.png')}
+        style={styles.imgFundo}>
+        <Image source={require('../assets/images/FindCep.png')} style={styles.logo}>
+        </Image>
+      </ImageBackground>
+      {/* sempre que você colocar uma imagem de fundo use essa tag de cima, o ImagemBackground */}
+
+      {/* 2. campo de consulta */}
+      <View style={styles.container}>
+        {/* 2.1 título */}
+        <Text style={styles.titulo}>Consulte deu CEP</Text>
+        {/* 2.2 input */}
+        <Input/>
+        {/* 2.3 botão */}
+        {/* 2.4 card de informações */}
+
+      </View>
+
+    </>
+  );
+}
+
+//estilos dos meus componentes:
+const styles = StyleSheet.create({
+  imgFundo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%',
+    width: '100%'
+  },
+  logo: {
+    width: 100,
+    height: 120
+  },
+  container:{
+    flex: 1.5,
+    alignItems: "center",
+    paddingTop: 50,
+    paddingBottom: 50,
+    gap: 40
+  },
+  titulo:{
+    fontSize: 25
+  }
+})
